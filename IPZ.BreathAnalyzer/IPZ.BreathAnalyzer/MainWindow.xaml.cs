ab82b870adb3e086dbffbcc38f1210047fde7a1f
Nuvar <nuvar2011@gmail.com>
2017-04-07 11:43:05 +0300

    Added wavelet vizualization

--- IPZ.BreathAnalyzer/IPZ.BreathAnalyzer/MainWindow.xaml.cs
+++ IPZ.BreathAnalyzer/IPZ.BreathAnalyzer/MainWindow.xaml.cs
@@ -9,8 +9,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NAudio.Dsp;
+using NAudio.Wave;
+using OxyPlot;
 using WaveletLogic;
 
 namespace IPZ.BreathAnalyzer
@@ -25,7 +29,46 @@ namespace IPZ.BreathAnalyzer
             InitializeComponent();
 
             var test = new WaveletLogic.Wavelet();
-      test.wavelet_transformation();
+            using (WaveFileReader reader = new WaveFileReader("sample.wav"))
+            {
+                byte[] buffer = new byte[reader.Length];
+                int read = reader.Read(buffer, 0, buffer.Length);
+                short[] sampleBuffer = new short[read / 2];
+                Buffer.BlockCopy(buffer, 0, sampleBuffer, 0, read);
+                var points = new List<DataPoint>();
+                Func<double, int, double> blackman = (x, y) =>
+                {
+                    return 0.54 - 0.46 * Math.Cos(2.0 * Math.PI * x / 99);
+                };
+                double fr = 1.0/44000;
+                double ms = 0.001;
+                int step = (int) (ms/fr);
+                for (int i = 0; i < reader.TotalTime.Milliseconds; i++)
+                {
+                    points.Add(new DataPoint(Convert.ToDouble(i), Convert.ToDouble(sampleBuffer[i*step])));
+                }
+
+                mainPlot.DataContext = points;
+
+                var myPlot = new SurfacePlotModel();
+                var myPlot1 = new SurfacePlotModel();
+                wavelet.DataContext = myPlot;
+                wavelet1.DataContext = myPlot1;
+                var res = test.wavelet_transformation((int)reader.TotalTime.TotalMilliseconds, sampleBuffer, step, test.WAVE_wavelet, 100);
+                var res1 = test.wavelet_transformation((int)reader.TotalTime.TotalMilliseconds, sampleBuffer, step, test.FHAT_wavelet, 100);
+                double[,] arr = new double[(int)res.Max(f => f.X) + 1, (int)res.Max(f => f.Y) + 1];
+                double[,] arr1 = new double[(int)res1.Max(f => f.X) + 1, (int)res1.Max(f => f.Y) + 1];
+                foreach (Point3D point3D in res)
+                {
+                    arr[(int)point3D.X, (int)point3D.Y] = point3D.Z;
+                }
+                foreach (Point3D point3D in res1)
+                {
+                    arr1[(int)point3D.X, (int)point3D.Y] = point3D.Z;
+                }
+                myPlot.PlotFunction((x, y) => arr[(int)x, (int)y], res.Select(r => r.X).ToArray(), res.Select(r => r.Y).ToArray());
+                myPlot1.PlotFunction((x, y) => arr1[(int)x, (int)y], res1.Select(r => r.X).ToArray(), res1.Select(r => r.Y).ToArray());
+            }
         }
     }
 }
--- IPZ.BreathAnalyzer/IPZ.BreathAnalyzer/WaveletLogic.cs
+++ IPZ.BreathAnalyzer/IPZ.BreathAnalyzer/WaveletLogic.cs
@@ -1,118 +1,119 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
+using System.Windows.Media.Media3D;
 
 namespace WaveletLogic
 {
-  // all code will be here for now
-  // todo restructure later
-  public class Wavelet
-  {
-    const int k = 200;
-    const double Period = 2 * Math.PI;
-    double[] mas_initial_chart = new double[500];
-    double[] mass_WAVE_wavelet = new double[500];
-    double[] mass_FHAT_wavelet = new double[500];
-    double[,] mass = new double[500, 500];
-    double[,] mass_drawing = new double[500, 500];
-    double step = Period / (k - 1);
-    double max = 0;
-    double min = 0;
-    double coefficient = 0;
-
-    //Wave-вейвлет
-    private double WAVE_wavelet(double t)
+    // all code will be here for now
+    // todo restructure later
+    public class Wavelet
     {
-      return -1 / Math.Sqrt(Period) * t * Math.Exp(-Math.Pow(t, 2) / 2);
+        const int k = 200;
+        const double Period = 2 * Math.PI;
+        double[] mas_initial_chart = new double[500];
+        double[] mass_WAVE_wavelet = new double[500];
+        double[] mass_FHAT_wavelet = new double[500];
+
+        double[,] mass_drawing = new double[500, 500];
+        double step = Period / (k - 1);
+        double max = 0;
+        double min = 0;
+        double coefficient = 0;
+
+        //Wave-вейвлет
+        public double WAVE_wavelet(double t, int T)
+        {
+            return -1 / Math.Sqrt(T) * t * Math.Exp(-Math.Pow(t, 2) / 2);
+        }
+
+        //Вейвлет "Мексиканская шляпа"
+        public double FHAT_wavelet(double t, int T)
+        {
+            return -1 / Math.Sqrt(T) * (Math.Pow(t, 2) - 1) * Math.Exp(-Math.Pow(t, 2) / 2);
+        }
+
+        /// <summary>
+        /// amplitude or any signal representation as the returned result will work 
+        /// </summary>
+        /// <param name="t">time</param>
+        /// <returns>in example it returned amplitude of a signal</returns>
+        private double GetSignalQuantum(double t)
+        {
+            // TEMPORARY FUNCTION
+            // TODO signal processing
+            return Math.Cos(Math.Pow(Math.E, Math.Sin(3 * t * 0.33)));
+        }
+
+        //Вейвлет-преобразование
+        public List<Point3D> wavelet_transformation(int T, short[] signal, int stp, Func<double, int, double> wavelet, int N)
+        {
+            int Tao = 0;
+            int S = 0;
+            double little_step = (1 - 0.01) / (k - 1);
+
+            int st = T / N;
+            if (st == 0)
+            {
+                st = 1;
+            }
+            List<Point3D> points = new List<Point3D>();
+            int Tau = 0;
+            int Ss = 0;
+            int Tt = 0;
+            try
+            {
+
+
+                for (int tau = 0; tau < T; tau += st)
+                {
+                    Tau = tau;
+                    for (int s = 1; s <= 500; s += 49)
+                    {
+                        Ss = s;
+                        var p = new Point3D(tau, s, 0.0);
+                        for (int t = 0; t <= T; t += st)
+                        {
+                            Tt = t;
+                            p.Z += Math.Abs(1.0 / Math.Sqrt(Math.Abs(s)) * signal[t * stp] * wavelet((double)(t - tau) / s, T));
+                        }
+                        points.Add(p);
+                        S++;
+                    }
+
+                    Tao++;
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            
+
+            // idk normalizing is unnesessary right now
+            #region SAVE_FOR_LATER 
+
+            min = points.Min(p => p.Z);
+            max = points.Max(p => p.Z);
+
+            coefficient = 255 / max;
+            for (int i = 0; i < points.Count; i++)
+            {
+                Point3D p = points[i];
+                p.Z -= min;
+                p.Z *= coefficient;
+                points[i] = p;
+            }
+            #endregion
+
+            int breakpointPlease = 0;
+
+            return points;
+        }
+
+
+
     }
-
-    //Вейвлет "Мексиканская шляпа"
-    private double FHAT_wavelet(double t)
-    {
-      return -1 / Math.Sqrt(Period) * (Math.Pow(t, 2) - 1) * Math.Exp(-Math.Pow(t, 2) / 2);
-    }
-
-    /// <summary>
-    /// amplitude or any signal representation as the returned result will work 
-    /// </summary>
-    /// <param name="t">time</param>
-    /// <returns>in example it returned amplitude of a signal</returns>
-    private double GetSignalQuantum(double t)
-    {
-      // TEMPORARY FUNCTION
-      // TODO signal processing
-      return Math.Cos(Math.Pow(Math.E, Math.Sin(3 * t * 0.33)));
-    }
-
-    //Вейвлет-преобразование
-    public void wavelet_transformation()
-    {
-      int Tao = 0;
-      int S = 0;
-      double little_step = (1 - 0.01) / (k - 1);
-
-      for (double tau = -Period; tau <= Period; tau += step)
-      {
-        S = 0;
-
-        for (double s = 0.01; s <= 1; s += little_step)
-        {
-          for (double t = -Period; t <= Period; t += step)
-            mass[Tao, S] += 1.0 / Math.Sqrt(Math.Abs(s)) * GetSignalQuantum(t) * WAVE_wavelet((t - tau) / s);
-
-          S++;
-        }
-
-        Tao++;
-      }
-
-      // idk normalizing is unnesessary right now
-      #region SAVE_FOR_LATER 
-      /*
-      for (int i = 0; i < k; i++)
-      {
-        for (int j = 0; j < k; j++)
-        {
-          if (min > mass[i, j])
-          {
-            min = mass[i, j];
-          }
-
-        }
-      }
-
-      for (int i = 0; i < k; i++)
-      {
-        for (int j = 0; j < k; j++)
-        {
-          mass[i, j] = mass[i, j] - min;
-        }
-      }
-
-      for (int i = 0; i < k; i++)
-      {
-        for (int j = 0; j < k; j++)
-        {
-          if (max < mass[i, j])
-          {
-            max = mass[i, j];
-          }
-
-        }
-      }
-
-      coefficient = 255 / max;
-      for (int i = 0; i < k; i++)
-      {
-        for (int j = 0; j < k; j++)
-        {
-          mass[i, j] = mass[i, j] * coefficient;
-
-        }
-      }
-    */
-      #endregion
-
-      int breakpointPlease = 0;
-    }
-
-  }
 }
